42f5a132e5513ed24e36a6f2c9808c055321fc30
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-07-16 06:12:00 +0530

    Easy POTD on GFG

---
+++ Remaining_String.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* https://www.geeksforgeeks.org/problems/remaining-string3515/1 */
+
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        for(int i=0;i<s.length();i++){
+            if(s[i] == ch){
+                count--;
+            }
+            
+            if(count == 0){
+                if(i == (s.length()-1)) return "";
+                else return s.substr(i+1,s.length());
+            }
+        }
+        
+        return "";
+    }
+};
